57c5cd3518ed18100e31e312c068a5e9b845df28
Tauan <tauansantosteles98@gmail.com>
2024-05-06 15:29:44 -0300

    Atividade-Smart-Tv

---
+++ src/tipos-e-variaveis/src/Incremento.java
@@ -0,0 +1,19 @@
+public class Incremento {
+
+public static void main(String[] args) {
+   // int numero=5;
+
+    //numero--;
+
+       // System.out.println(numero);
+
+       // a variavel comtinua verdadeira, só mudou em memoria. uma forma de transformar a variavel em falsa é aplicando p "!"
+
+       boolean variavel = false; 
+
+       System.out.println(!variavel);
+
+}
+
+
+}
---
+++ src/tipos-e-variaveis/src/Operador.java
@@ -0,0 +1,61 @@
+public class Operador {
+
+    public static void main(String[] args) {
+        
+    
+    // classe Operadores.java
+//int a, b;
+
+//a = 6;
+//b = 6;
+
+//String resultado = "";
+// EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+//if(a==b)
+  // resultado = "verdadeiro";
+//else
+  // resultado = "falso";
+
+
+//MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+//String resultado = (a==b) ? "verdadeiro" : "falso";
+
+//System.out.println(resultado);
+//classe Operadores.java
+//int numero1 = 1;
+//int numero2 = 2;
+
+       // boolean simNao= numero1 == numero2;
+
+        //System.out.println("numero1 é igual ao numero2?" + simNao);
+
+//if(numero1 > numero2)
+//	System.out.print("Numero 1 maior que numero 2");
+
+//if(numero1 < numero2)
+//	System.out.print("Numero 1 menor que numero 2");
+
+//if(numero1 >= numero2)
+//	System.out.print("Numero 1 maior ou igual que numero 2");
+
+//if(numero1 <= numero2)
+	//System.out.print("Numero 1 menor ou igual que numero 2");
+
+//if(numero1 != numero2)
+	//System.out.print("Numero 1 é diferente de numero 2");
+
+    boolean condicao1 = true;
+    boolean condicao2 = true; 
+
+    if (condicao1 && condicao2){
+        System.out.println("As duas Condições são verdadeiras");
+    }
+
+    if (condicao1 || condicao2){
+        System.out.println("Uma das condições é verdadeiras");
+    }
+
+    System.out.println("Fim");
+    }
+    
+}
